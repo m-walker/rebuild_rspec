e18fa445e8f0ad75ad1a3813c383aefa7ced4768
m-walker <michaelwalker610@gmail.com>
2017-11-09 11:58:08 -0500

    create grocery items and grocery cart

---
+++ groceries.rb
@@ -0,0 +1,24 @@
+class GroceryItem
+  attr_reader :price
+  def initialize(price)
+    @price = price
+  end
+end
+
+class Banana < GroceryItem
+end
+
+class Cantaloupe < GroceryItem
+end
+
+class GroceryCart
+  attr_reader :stuff
+  def initialize(stuff = [])
+    @stuff = stuff
+  end
+
+  def pick_out(item)
+    raise TypeError unless item.is_a?(GroceryItem)
+    stuff << item
+  end
+end
