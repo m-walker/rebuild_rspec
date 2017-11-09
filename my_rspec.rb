4f9c5a3e560962ebd04d3cee7f729f998019e917
m-walker <michaelwalker610@gmail.com>
2017-11-09 17:24:28 -0500

    implement let

--- my_rspec.rb
+++ my_rspec.rb
@@ -10,6 +10,7 @@ class DescribeBlock
     @subject = subject
     @tests = tests
     @eager_lets = {}
+    @lazy_lets = {}
     @let_values = {}
   end
 
@@ -22,19 +23,31 @@ class DescribeBlock
     Test.new(self, description, test).evaluate!
   end
 
-  def let!(variable_name, &blk)
-    @eager_lets[variable_name] = blk
+  def let(var_name, &blk)
+    @lazy_lets[var_name] = blk
+  end
+
+  def let!(var_name, &blk)
+    @eager_lets[var_name] = blk
   end
 
   def reevaluate_all_eager_lets
-    @let_values = {}
+    @eager_lets.keys.each { |var_name| @let_values.delete(var_name) }
     @eager_lets.each do |var, blk|
       @let_values[var] = blk.call
     end
   end
 
   def has_let_variable?(var_name)
-    @let_values.has_key?(var_name)
+    is_evaluated = @let_values.has_key?(var_name)
+    return is_evaluated if is_evaluated
+
+    if @lazy_lets.include?(var_name)
+      @let_values[var_name] = @lazy_lets[var_name].call
+      true
+    else
+      false
+    end
   end
 
   def get_variable(var_name)
